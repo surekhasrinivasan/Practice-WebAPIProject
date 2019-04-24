68d53423fd213b494e2d0e703195cdb93277c91e
Surekhasrinivasan <surekhasrao@gmail.com>
2019-04-23 21:36:43 -0400

    Code added for Product.cs

--- FirstWebAPIService/Models/Product.cs
+++ FirstWebAPIService/Models/Product.cs
@@ -7,5 +7,9 @@ namespace FirstWebAPIService.Models
 {
     public class Product
     {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string ProductCategory { get; set; }
+        public int Price { get; set; }
     }
 }
